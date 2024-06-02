dd4a9b00c02ec8f0370a565d1d21bff8c83e2d68
Marta Ercegović <marta.ercegovic@skole.hr>
2024-06-02 16:19:30 +0200

    izgled forme za proizvode gotovo

--- PrirodnaLjekarnaa/Form1.Designer.cs
+++ PrirodnaLjekarnaa/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PrirodnaLjekarnaa
 {
-    partial class Form1
+    partial class proizvodi_form
     {
         /// <summary>
         /// Required designer variable.
@@ -30,16 +30,31 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.naslovnicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.proizvodiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.doktorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pomoćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.oNamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ispisKupacaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proizvodiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.košaricaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dodajNovoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dodajNovoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.doktorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.najčešćeBolestiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomoćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oNamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -65,6 +80,12 @@
             this.naslovnicaToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
             this.naslovnicaToolStripMenuItem.Text = "Naslovnica";
             // 
+            // ispisKupacaToolStripMenuItem
+            // 
+            this.ispisKupacaToolStripMenuItem.Name = "ispisKupacaToolStripMenuItem";
+            this.ispisKupacaToolStripMenuItem.Size = new System.Drawing.Size(176, 26);
+            this.ispisKupacaToolStripMenuItem.Text = "Ispis kupaca ";
+            // 
             // proizvodiToolStripMenuItem
             // 
             this.proizvodiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -75,6 +96,24 @@
             this.proizvodiToolStripMenuItem.Size = new System.Drawing.Size(85, 24);
             this.proizvodiToolStripMenuItem.Text = "Proizvodi";
             // 
+            // košaricaToolStripMenuItem
+            // 
+            this.košaricaToolStripMenuItem.Name = "košaricaToolStripMenuItem";
+            this.košaricaToolStripMenuItem.Size = new System.Drawing.Size(170, 26);
+            this.košaricaToolStripMenuItem.Text = "Košarica";
+            // 
+            // dodajNovoToolStripMenuItem
+            // 
+            this.dodajNovoToolStripMenuItem.Name = "dodajNovoToolStripMenuItem";
+            this.dodajNovoToolStripMenuItem.Size = new System.Drawing.Size(170, 26);
+            this.dodajNovoToolStripMenuItem.Text = "Kupi";
+            // 
+            // dodajNovoToolStripMenuItem1
+            // 
+            this.dodajNovoToolStripMenuItem1.Name = "dodajNovoToolStripMenuItem1";
+            this.dodajNovoToolStripMenuItem1.Size = new System.Drawing.Size(170, 26);
+            this.dodajNovoToolStripMenuItem1.Text = "Dodaj novo";
+            // 
             // doktorToolStripMenuItem
             // 
             this.doktorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -82,6 +121,12 @@
             this.doktorToolStripMenuItem.Name = "doktorToolStripMenuItem";
             this.doktorToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
             this.doktorToolStripMenuItem.Text = "Doktor";
+            // 
+            // najčešćeBolestiToolStripMenuItem
+            // 
+            this.najčešćeBolestiToolStripMenuItem.Name = "najčešćeBolestiToolStripMenuItem";
+            this.najčešćeBolestiToolStripMenuItem.Size = new System.Drawing.Size(204, 26);
+            this.najčešćeBolestiToolStripMenuItem.Text = "Najčešće bolesti ";
             // 
             // pomoćToolStripMenuItem
             // 
@@ -95,47 +140,133 @@
             this.oNamaToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
             this.oNamaToolStripMenuItem.Text = "O nama ";
             // 
-            // ispisKupacaToolStripMenuItem
+            // listBox1
             // 
-            this.ispisKupacaToolStripMenuItem.Name = "ispisKupacaToolStripMenuItem";
-            this.ispisKupacaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.ispisKupacaToolStripMenuItem.Text = "Ispis kupaca ";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 308);
+            this.listBox1.TabIndex = 1;
             // 
-            // košaricaToolStripMenuItem
+            // textBox1
             // 
-            this.košaricaToolStripMenuItem.Name = "košaricaToolStripMenuItem";
-            this.košaricaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.košaricaToolStripMenuItem.Text = "Košarica";
+            this.textBox1.Location = new System.Drawing.Point(12, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 30);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Search nekakvi";
             // 
-            // dodajNovoToolStripMenuItem
+            // panel1
             // 
-            this.dodajNovoToolStripMenuItem.Name = "dodajNovoToolStripMenuItem";
-            this.dodajNovoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.dodajNovoToolStripMenuItem.Text = "Kupi";
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(290, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 344);
+            this.panel1.TabIndex = 3;
             // 
-            // dodajNovoToolStripMenuItem1
+            // label3
             // 
-            this.dodajNovoToolStripMenuItem1.Name = "dodajNovoToolStripMenuItem1";
-            this.dodajNovoToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
-            this.dodajNovoToolStripMenuItem1.Text = "Dodaj novo";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Opis:";
             // 
-            // najčešćeBolestiToolStripMenuItem
+            // label1
             // 
-            this.najčešćeBolestiToolStripMenuItem.Name = "najčešćeBolestiToolStripMenuItem";
-            this.najčešćeBolestiToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.najčešćeBolestiToolStripMenuItem.Text = "Najčešće bolesti ";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime:";
             // 
-            // Form1
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(274, 107);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(151, 144);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(274, 45);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 56);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(274, 8);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 248);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kupi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Dodaj u košaricu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(196, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 40);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ljekovita svojstva:";
+            // 
+            // proizvodi_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "proizvodi_form";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +285,19 @@
         private System.Windows.Forms.ToolStripMenuItem najčešćeBolestiToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem pomoćToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem oNamaToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PrirodnaLjekarnaa/Form1.cs
+++ PrirodnaLjekarnaa/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace PrirodnaLjekarnaa
 {
-    public partial class Form1 : Form
+    public partial class proizvodi_form : Form
     {
-        public Form1()
+        public proizvodi_form()
         {
             InitializeComponent();
         }
+
+        
     }
 }
