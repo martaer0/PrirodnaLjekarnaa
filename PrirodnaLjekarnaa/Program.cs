f60955a6873ddddc618bd9a88fda2ec47a07a3fd
Marta Ercegović <marta.ercegovic@skole.hr>
2024-06-16 01:07:44 +0200

    preimenovala 2

--- PrirodnaLjekarnaa/Program.cs
+++ PrirodnaLjekarnaa/Program.cs
@@ -16,7 +16,7 @@ namespace PrirodnaLjekarnaa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Proizvodi());
+            Application.Run(new NaslovnaStranicaPL());
         }
     }
 }
